89233df921ca6f8f77a67c82b97852ae96f9ca37
chenlinghy <chenlinghy@cmhi.chinamobile.com>
2021-03-11 14:46:43 +0800

    Spring Boot Devtools热部署 修改redis连接超时时间

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,22 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 启用缓存注解
+ *
  * @author Nicole
  */
 @EnableCaching
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+    @RequestMapping("/devtools")
+    String index() {
+        return "hello cary";
+    }
 
     public static void main(String[] args) {
 
